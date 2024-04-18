00753ab9a71802b56d41b70d5765e9f15f50a13d
ghjgjgjgjgjgjgj <fedkin.jr@gmail.com>
2024-04-18 17:10:36 +0200

    т

--- проект/bot.py
+++ проект/bot.py
@@ -5,7 +5,7 @@ from telebot import types
 import json2
 import requests
 from bs4 import BeautifulSoup
-
+from datetime import datetime, date, timedelta
 access_key = '8b1ee17f-80b6-431c-b9ac-e27f7c6e0dc0'
 
 file = open("works.json", "r", encoding="utf-8")
@@ -31,6 +31,11 @@ osad_types = {
     "light-rain": "легкий дождь"
 }
 
+def date_diff(day):
+    date_1 = date.today()
+    end_date = date_1 + timedelta(days=day)
+    return end_date
+
 
 access_key = 'a82ab972-523c-4643-b46d-5425f05a9c06'
 
@@ -39,9 +44,9 @@ pogod = response.json()
 data = []
 
 for i in range(7):
-    date = pogod['forecasts'][i]['date']
-    data.append(date)
-
+    date_1 = pogod['forecasts'][i]['date']
+    data.append(date_1)
+# print(data)
 
 token = '7086983775:AAF7uz_JgFMI-9xCQhdath-zW99nt5V6ZCg'
 bot = telebot.TeleBot(token)
@@ -50,7 +55,7 @@ def get_message(message):
     # Если текст полученного сообщения = '/start'
     if message.text == '/start':
         keyboard = types.InlineKeyboardMarkup()
-        button1 = types.InlineKeyboardButton(text=date, callback_data='1 день')
+        button1 = types.InlineKeyboardButton(text=data[0], callback_data='1 день')
         button2 = types.InlineKeyboardButton(text=data[1], callback_data='2 день')
         button3 = types.InlineKeyboardButton(text=data[2], callback_data='3 день')
         button4 = types.InlineKeyboardButton(text=data[3], callback_data='4 день')
@@ -62,20 +67,29 @@ def get_message(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
 
-    print('call.data')
-    print(call.data)
+    # print('call.data')
+    # print(call.data)
     if call.data in['1 день','2 день','3 день' ,'4 день','5 день','6 день','7 день'] :
         day = int(call.data.split(' ')[0]) - 1
         temp = pogod['forecasts'][day]['parts']['day_short']['temp']
         osad = pogod['forecasts'][day]['parts']['day_short']['condition']
-        rabota = grafik["work"]
+        # rabota = grafik["work"]
         # glav = grafik["worker"]
         message_text = ""
-        if grafik['date'] == date:
-         message_text = "Температура:" + str(temp) + "\n" + "Осадки:" + osad_types[osad] + "\n" + "Работа:" + rabota
+        for i in range(len(grafik)):
+          # print(type(grafik[i]['date']), type(date_diff(day)), grafik[i]['date'] == date_diff(day))
+          if grafik[i]['date'] == str(date_diff(day)):
+              message_text = "Температура:" + str(temp) + "\n" + "Осадки:" + osad_types[osad] + "\n" + "Работа:" + grafik[i]["work"]
+              break
+          else:
+              message_text = "Температура:" + str(temp) + "\n" + "Осадки:" + osad_types[osad] + "\n" + "Работа:" + "Работа в этот день не назначена"
+
+
 
         if message_text != '':
-            bot.send_message(call.from_user.id, text= message_text)
+            bot.send_message(call.from_user.id, text=message_text)
+        else:
+            bot.send_message(call.from_user.id, text = "Данные за день не найдены")
 
 
 
